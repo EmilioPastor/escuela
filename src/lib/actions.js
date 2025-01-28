fa1f436325cd82a3c5c0be892bc6d0c0976915d5
EmilioPastor <emiliopastorzurita906@gmail.com>
2025-01-28 16:46:27 +0100

    Grupos

---
+++ src/lib/actions.js
@@ -0,0 +1,53 @@
+'use server'
+import { PrismaClient } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+
+const prisma = new PrismaClient();
+
+//--------------------------grupos------------------------------------
+
+export async function insertarGrupo(formData){
+    const nombre = formData.get('nombre');
+    const tutor = formData.get('tutor');
+    const aula = formData.get('aula');
+    await prisma.grupo.create({
+        data: {
+            nombre:nombre, 
+            tutor: tutor,
+            aula: aula
+        }
+    });
+    revalidatePath('/grupos');
+}
+
+
+export async function modificarGrupo(formData){
+    const id = Number(formData.get('id'));
+    const nombre = formData.get('nombre');
+    const tutor = formData.get('tutor');
+    const aula = formData.get('aula');
+    await prisma.grupo.update({
+        where:{
+            id: id
+        },
+        data: {
+            id: id,
+            nombre:nombre, 
+            tutor: tutor,
+            aula: aula
+        }
+    });
+    revalidatePath('/grupos');
+}
+
+
+
+export async function eliminarGrupo(formData){
+    const id = Number(formData.get('id'))
+    await prisma.grupo.delete({
+        where:{
+            id: id
+        }
+    });
+    revalidatePath('/grupos');
+}
